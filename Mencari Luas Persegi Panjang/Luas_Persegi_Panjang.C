819003ca277c6e8180f6479ba5db7d8732e63e00
Xsilaban <45526337+Xsilaban@users.noreply.github.com>
2018-12-03 01:47:17 +0700

    Update Luas_Persegi_Panjang.C

--- Mencari Luas Persegi Panjang/Luas_Persegi_Panjang.C
+++ Mencari Luas Persegi Panjang/Luas_Persegi_Panjang.C
@@ -4,8 +4,10 @@ int main ()
     int panjang, lebar, luas;
     printf ("\tMenghitung Luas Persegi Panjang");
     printf ("\n");
-    printf ("\nMasukkan Panjang : "); scanf ("%d",&panjang);
-    printf ("\nMasukkan Lebar   : "); scanf ("%d",&lebar);
+    printf ("\nMasukkan Panjang : ");
+    scanf ("%d",&panjang);
+    printf ("\nMasukkan Lebar   : ");
+    scanf ("%d",&lebar);
     luas=panjang * lebar;
     printf ("\nMaka Luas Persegi Panjang Adalah : %d",luas);
     printf ("\n");
