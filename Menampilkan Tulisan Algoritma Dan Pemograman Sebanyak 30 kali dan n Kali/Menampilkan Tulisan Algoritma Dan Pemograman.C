443987766dfaa4b0c4b79fab97dbde09143c3d1c
Xsilaban <45526337+Xsilaban@users.noreply.github.com>
2018-12-03 03:31:44 +0700

    Create Menampilkan Tulisan Algoritma Dan Pemograman.C

---
+++ Menampilkan Tulisan Algoritma Dan Pemograman Sebanyak 30 kali dan n Kali/Menampilkan Tulisan Algoritma Dan Pemograman.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    char n;
+    int m, i ;
+    printf("Berapa Kali Yang Ingin Diulang ?  : ");
+    scanf ("%d", &m);
+    if (i==n)
+        printf("i<=i");
+    for (i=1; i<=m; i++)
+        printf("Algoritma Dan Pemrograman\n", i);
+        return 0;
+}
