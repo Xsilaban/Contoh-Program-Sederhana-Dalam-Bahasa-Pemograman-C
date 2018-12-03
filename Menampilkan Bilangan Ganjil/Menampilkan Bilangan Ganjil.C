de91a88a9607e891b6d9234b6cc86d06cacfe1a1
Xsilaban <45526337+Xsilaban@users.noreply.github.com>
2018-12-03 22:49:12 +0700

    Create Menampilkan Bilangan Ganjil.C

---
+++ Menampilkan Bilangan Ganjil/Menampilkan Bilangan Ganjil.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main()
+ {
+  int i, n, x, y, m;
+  printf ("\tMENAMPILKAN BILANGAN GANJIL");
+  printf("\nMasukkan Nilai n : ");
+  scanf ("%d", &n);
+  for (i=1;i<=n;i+=2)
+  printf("%2d,", i);
+  printf("\n");
+ }
